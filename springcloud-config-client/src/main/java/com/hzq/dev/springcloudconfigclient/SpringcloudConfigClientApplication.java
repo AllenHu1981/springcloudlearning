1a4b85a594c25e68aee3be1ec62211ae72eff7dd
Allen <39222727@qq.com>
2018-05-23 15:27:39 +0800

    feat:add springcloud-config-client

---
+++ springcloud-config-client/src/main/java/com/hzq/dev/springcloudconfigclient/SpringcloudConfigClientApplication.java
@@ -0,0 +1,25 @@
+package com.hzq.dev.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringcloudConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "hi")
+    public  String hi(@RequestParam String name){
+        return foo +"   "+name;
+    }
+}
