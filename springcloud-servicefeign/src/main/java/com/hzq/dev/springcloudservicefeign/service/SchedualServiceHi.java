8bd345f33935e65a972a7562012cb8fbbbc36517
huzongquan <39222727@qq.com>
2018-06-21 18:27:22 +0800

    feak:add config eurekaserver

--- springcloud-servicefeign/src/main/java/com/hzq/dev/springcloudservicefeign/service/SchedualServiceHi.java
+++ springcloud-servicefeign/src/main/java/com/hzq/dev/springcloudservicefeign/service/SchedualServiceHi.java
@@ -1,14 +1,16 @@
 package com.hzq.dev.springcloudservicefeign.service;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//@FeignClient(value = "service-client",fallback = SchedualServiceHiHytrix.class)
-@FeignClient(value = "${project.eurekaclient.name}",fallback = SchedualServiceHiHytrix.class)
+@Service
+@FeignClient(value = "service-client",fallback = SchedualServiceHiHytrix.class)
+//@FeignClient(value = "${project.eurekaclient.name}",fallback = SchedualServiceHiHytrix.class)
 public interface SchedualServiceHi {
 
     @RequestMapping(value = "hi",method = RequestMethod.GET)
-    public String sayHiFromClientOne(@RequestParam(value = "name") String name);
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
 }
